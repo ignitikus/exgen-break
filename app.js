49c9dbbbdaf9023ac654411e644c2cc42ae64987
NikolayK <nikolay.kim@codeimmersive.com>
2020-02-25 10:24:41 -0500

    adds files

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouters = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouters = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'house'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -38,4 +30,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use('/', indexRouters);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(next, req, resxt) {
-  res.render('/index', { title: 'Express' });
+router.get('/', function(req,res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+router.get('/layout', (req,res) => {
+  res.render('layout', {title: 'layout'})
+}
+)
 
 module.exports = router;
